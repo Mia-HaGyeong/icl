964367aac1071fe92e7a0ad58116a8f253da56d0
Mia-HaGyeong <129933771+Mia-HaGyeong@users.noreply.github.com>
2023-07-06 11:01:00 +0900

    Update transformers_semiar_assignment.py

--- transformers_semiar_assignment.py
+++ transformers_semiar_assignment.py
@@ -2,6 +2,11 @@ import streamlit as st
 from transformers import AutoTokenizer, AutoModel
 import torch
 
+def mean_pooling(model_output, attention_mask):
+    token_embeddings = model_output[0] # First element of model_output contains all token embeddings
+    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+
 def is_paraphrase(sentence1, sentence2):
     tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/paraphrase-xlm-r-multilingual-v1')
     model = AutoModel.from_pretrained('sentence-transformers/paraphrase-xlm-r-multilingual-v1')
